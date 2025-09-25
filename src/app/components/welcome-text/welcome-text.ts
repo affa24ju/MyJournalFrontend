f0343a5b358ad3a5b8c1afc3328a2cce5ab6f7ad
Fatema Afroza <fafroza2017@gmail.com>
2025-09-25 16:12:40 +0200

    La till input fälter & knapper i register form

--- src/app/components/welcome-text/welcome-text.ts
+++ src/app/components/welcome-text/welcome-text.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RegisterBtn } from '../register-btn/register-btn';
 import { LoginBtn } from '../login-btn/login-btn';
+import { RegisterForm } from '../register-form/register-form';
 
 @Component({
   selector: 'app-welcome-text',
-  imports: [RegisterBtn, LoginBtn],
+  imports: [RegisterBtn, LoginBtn, RegisterForm],
   templateUrl: './welcome-text.html',
   styleUrl: './welcome-text.css'
 })
