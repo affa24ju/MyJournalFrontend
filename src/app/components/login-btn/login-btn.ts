1fb7848a2fde3ce799be6c3d3f3790215bec03d5
Fatema Afroza <fafroza2017@gmail.com>
2025-09-25 11:54:35 +0200

    Skapat Login btn

---
+++ src/app/components/login-btn/login-btn.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-btn',
+  imports: [],
+  templateUrl: './login-btn.html',
+  styleUrl: './login-btn.css'
+})
+export class LoginBtn {
+
+}
