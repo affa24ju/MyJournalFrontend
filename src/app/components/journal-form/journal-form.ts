ef6b2337e5d2ecdb0921030cfcfaf578cb2b3d58
Fatema Afroza <fafroza2017@gmail.com>
2025-09-25 12:06:56 +0200

    Skapat journalForm component

---
+++ src/app/components/journal-form/journal-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-form',
+  imports: [],
+  templateUrl: './journal-form.html',
+  styleUrl: './journal-form.css'
+})
+export class JournalForm {
+
+}
