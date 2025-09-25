e0eaff9d0135b2a6a175af4dd9cc7ff293d1f2ce
Fatema Afroza <fafroza2017@gmail.com>
2025-09-25 21:19:26 +0200

    La till onSubmit() inne i register form

--- src/app/components/register-form/register-form.ts
+++ src/app/components/register-form/register-form.ts
@@ -1,14 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-form.html',
   styleUrl: './register-form.css'
 })
 export class RegisterForm {
+  username = '';
+  password = '';
 
   @Output() close = new EventEmitter<void>();
+  @Output() register = new EventEmitter<{username: string, password: string}>();
+
+  @Input() 
 
   closeForm() {
     console.log("Close register form");
@@ -16,7 +22,7 @@ export class RegisterForm {
   }
   onSubmit() {
     console.log("Form submitted");
+    this.register.emit({username: this.username, password: this.password});
     this.close.emit();
   }
-
 }
