4db0d31fcab6d34d946f322573cc93e10115d7ab
Fatema Afroza <fafroza2017@gmail.com>
2025-09-25 12:04:21 +0200

    Skapat journalList component

---
+++ src/app/components/journal-list/journal-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-list',
+  imports: [],
+  templateUrl: './journal-list.html',
+  styleUrl: './journal-list.css'
+})
+export class JournalList {
+
+}
