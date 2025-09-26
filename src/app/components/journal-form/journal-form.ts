3bb8b1e2cfce49b034e34b20346bccc8a9b10b07
Fatema Afroza <fafroza2017@gmail.com>
2025-09-26 22:27:08 +0200

    Skapat onSubmit metod med bara log till journalForm

--- src/app/components/journal-form/journal-form.ts
+++ src/app/components/journal-form/journal-form.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-journal-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './journal-form.html',
   styleUrl: './journal-form.css'
 })
 export class JournalForm {
+  note = '';
+  feeling = 'GLAD'; // Default value
+
+  onSubmit() {
+    console.log("Journal form submitted");
+  }
 
 }
