1c60dac791f1802c74deaee4761a94ae5a0e9f9e
Fatema Afroza <fafroza2017@gmail.com>
2025-09-26 15:04:40 +0200

    placeholder till metoder closeForm & onSubmit för att få bort fel meddelande

--- src/app/components/login-form/login-form.ts
+++ src/app/components/login-form/login-form.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css'
 })
 export class LoginForm {
+  username = '';
+  password = '';
+
+  closeForm() {
+    console.log("Close login form");
+  }
+  onSubmit() {
+    console.log("Login form submitted");
+  }
 
 }
