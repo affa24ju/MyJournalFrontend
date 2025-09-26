50a66bf24469ffccdedc9371e75f7b28f4ca58e8
Fatema Afroza <fafroza2017@gmail.com>
2025-09-26 14:39:49 +0200

    La till loginform under welcome-text

--- src/app/components/welcome-text/welcome-text.ts
+++ src/app/components/welcome-text/welcome-text.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RegisterBtn } from '../register-btn/register-btn';
 import { LoginBtn } from '../login-btn/login-btn';
 import { RegisterForm } from '../register-form/register-form';
+import { LoginForm } from '../login-form/login-form';
 
 
 
 @Component({
   selector: 'app-welcome-text',
-  imports: [RegisterBtn, LoginBtn, RegisterForm],
+  imports: [RegisterBtn, LoginBtn, RegisterForm, LoginForm],
   templateUrl: './welcome-text.html',
   styleUrl: './welcome-text.css'
 })
 export class WelcomeText {
   showRegister = false;
-
+  showLogin = false;
 }
