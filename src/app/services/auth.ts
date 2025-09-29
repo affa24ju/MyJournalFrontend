ce894f36f9749e69097100b95ac1a33f9e875af8
Fatema Afroza <fafroza2017@gmail.com>
2025-09-29 15:54:15 +0200

    register method: för att skapa ny user

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 // Typ för svaret från backend vid inloggning/registrering, (AuthResponse i backend) 
 export interface AuthResponse {
@@ -11,5 +13,17 @@ export interface AuthResponse {
   providedIn: 'root'
 })
 export class Auth {
-  
+  // Bas-URL för backend API
+  private apiUrl = 'http://localhost:8080/api/auth'; 
+
+  constructor(private http: HttpClient) { }
+
+  // Metod för användarregistrering
+  register(username: string, password: string): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/register`, { username, password })
+    .pipe(tap(response => {
+      // Spara token i localStorage vid lyckad registrering
+      localStorage.setItem('token', response.token);
+    }));
+  }
 }
