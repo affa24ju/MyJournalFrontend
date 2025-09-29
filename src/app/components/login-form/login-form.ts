124d579f25f7830390c944ae695f1359a402b64f
Fatema Afroza <fafroza2017@gmail.com>
2025-09-29 20:24:42 +0200

    Uppdaterat onSubmit(), nu går att logga in

--- src/app/components/login-form/login-form.ts
+++ src/app/components/login-form/login-form.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
+
 
 @Component({
   selector: 'app-login-form',
@@ -13,22 +15,38 @@ export class LoginForm {
   password = '';
 
   @Output() close = new EventEmitter<void>();
-  @Output() login = new EventEmitter<{username: string, password: string}>();
+  // @Output() login = new EventEmitter<{username: string, password: string}>();
 
-  // Injecerara Router för navigering efter inloggning
-  constructor(private router: Router) {}
+  // Injecerara Router för navigering efter inloggning, & auth service för inloggning
+  constructor(private router: Router, private auth: Auth) {}
 
   closeForm() {
     console.log("Close login form");
     this.close.emit();
   }
   onSubmit() {
-    console.log("Login form submitted", this.username, this.password);
-    // Skicka inloggningsdata till föräldrakomponenten
-    this.login.emit({username: this.username, password: this.password});
-    this.close.emit();
+    // console.log("Login form submitted", this.username, this.password);
+    // // Skicka inloggningsdata till föräldrakomponenten
+    // this.login.emit({username: this.username, password: this.password});
+    // this.close.emit();
 
-    // Navigera till journal-sidan efter inloggning
-    this.router.navigate(['/journal']);
+    // // Navigera till journal-sidan efter inloggning
+    // this.router.navigate(['/journal']);
+
+    // Anropar backend för inloggning via Auth service
+    this.auth.login(this.username, this.password).subscribe({
+      next: (response) => {
+        console.log("Inloggning lyckades", response);
+        this.close.emit();
+        // Sparar token i localStorage
+        localStorage.setItem('token', response.token);
+        // Navigera till journal-sidan efter inloggning
+        this.router.navigate(['/journal']);
+      },
+      error: (error) => {
+        console.error("Inloggning misslyckades", error);
+        alert("Inloggning misslyckades: " + error.error.message);
+      }
+    });
   }
 }
