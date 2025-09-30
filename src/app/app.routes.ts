f921dec94886d3ebe92c7ced6d62e1b5cf16651b
Fatema Afroza <fafroza2017@gmail.com>
2025-09-30 20:36:37 +0200

    La till routing i app.routes.ts till statistics sidan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WelcomeText } from './components/welcome-text/welcome-text';
 import { JournalPage } from './pages/journal-page/journal-page';
+import { StatisticsPage } from './pages/statistics-page/statistics-page';
 
 export const routes: Routes = [
     { path: '', component: WelcomeText },
-    { path: 'journal', component: JournalPage} 
+    { path: 'journal', component: JournalPage},
+    { path: 'statistics', component: StatisticsPage} 
 ];
