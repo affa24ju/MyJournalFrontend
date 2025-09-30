03054343fd50a2fc9cc329faa4916d84025a124a
Fatema Afroza <fafroza2017@gmail.com>
2025-09-30 09:42:15 +0200

    createEntry metod inne i service

--- src/app/services/journl-service.ts
+++ src/app/services/journl-service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 // Interface för att ta emot Journal entries, motsvarar JournalEntryRequest dto i backend
 export interface JournalEntryRequest {
@@ -17,5 +19,15 @@ export interface JournalEntry {
   providedIn: 'root'
 })
 export class JournlService {
+  // Bas-URL för backend API
+  private apiUrl = 'http://localhost:8080/api/myJournal'; 
+
+  constructor(private http: HttpClient) { }
+
+  // Metod för att skapa en ny journal entry
+  createEntry(request: JournalEntryRequest): Observable<JournalEntry> {
+    return this.http.post<JournalEntry>(`${this.apiUrl}/createJournalEntry`, request);
+
+  }
   
 }
