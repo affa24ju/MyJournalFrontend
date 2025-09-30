4e5ca62ee1d5879db0c81dcca100bdd3d08041da
Fatema Afroza <fafroza2017@gmail.com>
2025-09-30 11:44:55 +0200

    Implementerar OnInit inne i journal-list

--- src/app/components/journal-list/journal-list.ts
+++ src/app/components/journal-list/journal-list.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { JournalEntry, JournlService } from '../../services/journl-service';
 
 @Component({
   selector: 'app-journal-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './journal-list.html',
   styleUrl: './journal-list.css'
 })
-export class JournalList {
+export class JournalList implements OnInit {
+
+  entries : JournalEntry[] = [];
+  loading = true;
+
+  constructor(private journalService: JournlService) { }
+
+  ngOnInit() {
+    this.loadTodayEntries();
+  }
+
+  // Metod för att ladda dagens anteckningar
+  loadTodayEntries() {
+    
+  }
+    
+
 
 }
