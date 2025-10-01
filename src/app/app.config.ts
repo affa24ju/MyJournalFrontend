fcc0b5fa03181d8755e665e61fcaf18aced83512
Fatema Afroza <fafroza2017@gmail.com>
2025-10-01 19:55:13 +0200

    funkar diagram nu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,8 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth-interceptor';
 import { registerLocaleData } from '@angular/common';
 import localeSv from '@angular/common/locales/sv';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
 
 // Registrerar svenska som språk för datumformattering
 registerLocaleData(localeSv);
@@ -16,6 +18,9 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideCharts(withDefaultRegisterables()),
     { provide: LOCALE_ID, useValue: 'sv' }  // Sätter svenska som standard locale
   ]
 };
+
+
--- src/app/pages/statistics-page/statistics-page.ts
+++ src/app/pages/statistics-page/statistics-page.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FeelingService } from '../../services/feeling-service';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, Legend, plugins } from 'chart.js';
+
 
 @Component({
   selector: 'app-statistics-page',
-  imports: [FormsModule, RouterLink, CommonModule],
+  imports: [FormsModule, RouterLink, CommonModule, BaseChartDirective],
   templateUrl: './statistics-page.html',
   styleUrl: './statistics-page.css'
 })
@@ -16,8 +19,28 @@ export class StatisticsPage {
   endDate: string | null = null;
   stats: any = null;
 
+  //chart
+  chartData: ChartData<'pie', number[], string> = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: []
+      }
+    ]
+  };
+  chartOptions: ChartConfiguration<'pie'> ['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom' as const
+      }
+    }
+  };  
+
   constructor(private journalservice: JournlService, private feelingService: FeelingService) { }
 
+  // Metod för att visa statistiken
   loadStats(){
     if (!this.startDate || !this.endDate) {
       alert('Välj både start- och slutdatum!');
@@ -28,6 +51,23 @@ export class StatisticsPage {
       next: (data) => {
         console.log('Statistik data:', data);      
         this.stats = data;
+
+        // Till diagram
+        const labels = Object.keys(data.feelingPercentages);
+        const values = Object.values(data.feelingPercentages);
+
+        // Genererar slumpmässigt färg
+        const colors = labels.map(() => `hsl(${Math.random() * 360}, 70%, 60%)`);
+
+        this.chartData = {
+          labels,
+          datasets: [
+            {
+              data: values as number[],
+              backgroundColor: colors
+            }
+          ]
+        };
       },
       error: (err) => {
         console.error('Fel vid hämtning av statistik:', err);
@@ -45,5 +85,6 @@ export class StatisticsPage {
   getEmoji(feeling: any) {
     return this.feelingService.getEmoji(feeling);
   }
+
 
 }
