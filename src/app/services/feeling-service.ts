fcd47dc62e283fa744a41c4f233b833d43dda8be
Fatema Afroza <fafroza2017@gmail.com>
2025-10-02 09:48:10 +0200

    Städat & fixat lite stil grej

--- src/app/services/feeling-service.ts
+++ src/app/services/feeling-service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 
+// Den här service filen används för att visa emojis inne journal-list, 
+// journal-history och statistic-page
 @Injectable({
   providedIn: 'root'
 })
 export class FeelingService {
   private emojis: Record<string, string> = {
+    // Skapar emojis: samma som enum klass Feeling i backenden
+    // Emojis har jag bara i frontened, i databas sparas bara en sträng
     GLAD: '😊',
     SAD: '😢',
     ANGRY: '😡',
@@ -20,6 +24,8 @@ export class FeelingService {
     HOPEFUL: '🤞'
   };
 
+  // Tar in feeling & returnerar respektiv emoji
+  // Om det inte finns i listan, returnerar ?
   getEmoji(feeling: any): string {
     return this.emojis[feeling] || '❓';
   }
